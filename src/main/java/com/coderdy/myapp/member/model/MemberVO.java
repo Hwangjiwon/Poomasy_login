802178fa4bb38f67b3fe79dd25b102e28ece690f
jiwon <googims33@naver.com>
2019-01-24 12:04:54 +0900

    MemberVO
    Mysql + Mybatis 연동, DataSource Test, MemberVO (회원 정보 기능 선언)

--- src/main/java/com/coderdy/myapp/member/model/MemberVO.java
+++ src/main/java/com/coderdy/myapp/member/model/MemberVO.java
@@ -1,5 +1,40 @@
 package com.coderdy.myapp.member.model;
 
 public class MemberVO {
-
+	public String userid;
+	public String password;
+	public String email;
+	public String name;
+	public String phone;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
